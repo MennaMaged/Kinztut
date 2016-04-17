ff1b42b474ac18ee8fb5bb3eaf83b8f13f8b5f31
MennaMaged <menna_maged@yahoo.com>
2016-04-17 17:00:45 +0200

    test

--- Pages/HomePage.py
+++
@@ -1,9 +0,0 @@
-#import selenium
-#from selenium.webdriver.common.by import By
-
-#class FirstPage():
-    #SignIn_BTN = (By.LINK_TEXT, 'http://www.kinztut.com/login.php')  # Define Signin Button
-
-    #def SignIn(self):
-        #self.driver.find_element(*FirstPage.SignIn_BTN).click
-
--- TestCases/TestCase1.py
+++ TestCases/TestCase1.py
@@ -9,3 +9,5 @@ driver.get("http://www.kinztut.com/")
 
 SignIn_BTN = driver.find_element_by_xpath(".//*[@id='home']/div[4]/div[1]/div[2]/div/div/div[2]/ul/li[6]/div/a[1]")
 SignIn_BTN.click()
+
+driver.close()
