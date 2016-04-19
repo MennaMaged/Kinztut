f9e54c0aff329d2ef900381230148860d1e09a7e
MennaMaged <menna_maged@yahoo.com>
2016-04-19 12:02:15 +0200

    test

--- TestCases/TestCase1.py
+++ TestCases/TestCase1.py
@@ -1,13 +1,24 @@
+import unittest
 from selenium import webdriver
 
-fp = webdriver.FirefoxProfile('C:/Profiles')
-driver = webdriver.Firefox(fp)
-driver.implicitly_wait(30)
-driver.maximize_window()
+class TestCase1(unittest.TestCase):
+    def setUp(self):
 
-driver.get("http://www.kinztut.com/")
+        fp = webdriver.FirefoxProfile('C:/Profiles')
+        self.driver = webdriver.Firefox(fp)
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+        self.driver.get("http://www.kinztut.com/")
 
-SignIn_BTN = driver.find_element_by_xpath(".//*[@id='home']/div[4]/div[1]/div[2]/div/div/div[2]/ul/li[6]/div/a[1]")
-SignIn_BTN.click()
+    def test_signIn(self):
 
-driver.close()
+        signIn_BTN = self.driver.find_element_by_xpath(".//*[@id='home']/div[4]/div[1]/div[2]/div/div/div[2]/ul/li[6]/div/a[1]")
+        signIn_BTN.click()
+        signIn_title = self.driver.find_element_by_xpath(".//*[@id='sl-dialog-login']/div/div/div[1]/div").text
+        self.assertEqual(signIn_title,'Sign In')
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
