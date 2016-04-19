add65a0743bebe6d2e97cd4bb99cf129265ee96b
MennaMaged <menna_maged@yahoo.com>
2016-04-19 19:13:57 +0200

    test

--- TestCases/TestCase1.py
+++ TestCases/TestCase1.py
@@ -2,9 +2,11 @@ import unittest
 from selenium import webdriver
 
 class TestCase1(unittest.TestCase):
+
+    # we use classmethod to open the same instance of firefox for all tests
     @classmethod
     def setUp(cls):
-
+        # This line identifies the firefox profile to prevent the subscription step
         fp = webdriver.FirefoxProfile('C:/Profiles')
         cls.driver = webdriver.Firefox(fp)
         cls.driver.implicitly_wait(30)
@@ -17,19 +19,37 @@ class TestCase1(unittest.TestCase):
         signIn_btn.click()
         signIn_title = self.driver.find_element_by_xpath(".//*[@id='sl-dialog-login']/div/div/div[1]/div").text
         self.driver.implicitly_wait(30)
+
+        # comparing between the popup title and the expected title
         self.assertEqual(signIn_title,'Sign In')
+
         self.driver.implicitly_wait(30)
+
+        # identify and submit the email
         email_txt = self.driver.find_element_by_xpath(".//*[@id='customer_login']/input[1]")
         email_txt.clear()
         email_txt.send_keys("Kmonir@integrant.com")
+
+        #identify and submit the password
         pass_txt = self.driver.find_element_by_xpath(".//*[@id='customer_login']/input[2]")
         pass_txt.clear()
         pass_txt.send_keys("hello_1234")
+
+        # click on the submit button
         submit_btn = self.driver.find_element_by_xpath(".//*[@id='customer_login']/input[3]")
         submit_btn.click()
         self.driver.implicitly_wait(30)
+
+        # After login we check for a lable in the next page which prove that login is done successfully
         title_lbl = self.driver.find_element_by_xpath(".//*[@id='LayoutColumn2']/div/div/h1")
         self.assertTrue(title_lbl.is_displayed())
+        self.driver.implicitly_wait(30)
+
+        # To move to the next page 'Flavor' page
+        flavor_btn = self.driver.find_element_by_xpath(".//*[@id='SideCategoryList']/div/div/ul[1]/li[3]/a")
+        flavor_btn.click()
+        self.driver.implicitly_wait(30)
+
 
     @classmethod
     def tearDown(cls):
